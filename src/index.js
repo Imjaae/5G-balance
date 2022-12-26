eaebf9fdc3a4acd06e92b2fdfe84dbabd76834bb
songjihyun-dev <songjihyun323@gmail.com>
2022-12-26 17:12:24 +0900

    (1) 라우터 수정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+<<<<<<< Updated upstream
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+=======
+import { Reset } from "styled-reset";
+import App from "./App";
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <RouterProvider router={router} />
+=======
+    <Reset />
+    <App />
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
