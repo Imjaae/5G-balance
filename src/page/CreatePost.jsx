dafeab1d5c51b1891cff3f55261d16f5031d76ca
hotsix <codinggo@naver.com>
2022-12-27 19:39:21 +0900

    (9) merge

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Input from "../components/Input";
 import Content from "../components/Content";
 import Nickname from "../components/Nickname";
 import Swal from "sweetalert2";
+import axios from "axios";
 
 //닉네임 input box 유효성 검증
 export const nicknameCheck = (id) => {
@@ -32,7 +33,7 @@ const CreatePost = () => {
   console.log(balance);
 
   // 밸런스 게임 만들기, 설명 input box 유효성 검증
-  const handleContentsSubmit = () => {
+  const handleContentsSubmit = async () => {
     const A = balance.choice1.length;
     const B = balance.choice2.length;
     const C = balance.contents.length;
@@ -40,9 +41,11 @@ const CreatePost = () => {
     console.log(nicknameCheck(balance.nickname));
     if (!nicknameCheck(balance.nickname)) {
       window.alert("Nickname error");
+      return;
     }
     if (!passwordCheck(balance.password)) {
       window.alert("Password error");
+      return;
     }
     if (A < 1 || B < 1 || C < 1) {
       Swal.fire({
@@ -53,13 +56,32 @@ const CreatePost = () => {
         showConfirmButton: false,
         timerProgressBar: true,
       });
-    } else {
-      console.log("여기서 부터 이제 파베 연결");
+      return;
     }
-
+    const CreatePostData = {
+      choice1: balance.choice1,
+      choice2: balance.choice2,
+      contents: balance.contents,
+      nickname: balance.nickname,
+      password: balance.password,
+    };
+    await axios.post(
+      `https://json-server-vercel-mauve-nu.vercel.app/balances`,
+      CreatePostData
+    );
     console.log(balance);
   };
 
+  const getBalances = async () => {
+    const res = await axios.get(
+      "https://json-server-vercel-mauve-nu.vercel.app/balances"
+    );
+    console.log(res);
+    return res.data;
+  };
+  useEffect(() => {
+    getBalances();
+  }, []);
   return (
     <>
       <WWrap>
